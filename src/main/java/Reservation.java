71bc85c12bfe5756886ac596a4293c2e1e6bb9e1
Anagnostou-Ioannis <janagnostou@outlook.com>
2024-04-14 16:40:23 +0300

    Updated Class Reservation

--- src/main/java/Reservation.java
+++ src/main/java/Reservation.java
@@ -1,2 +1,29 @@
 public class Reservation {
+    private Insured insured;
+    private Timeslot timeslot;
+
+
+    //Reservation Constructor
+    public Reservation(Insured insured,Timeslot timeslot){
+        this.insured = insured;
+        this.timeslot = timeslot;
+    }
+
+    //Getters and Setters
+    public Insured getInsured() {
+        return insured;
+    }
+
+    public void setInsured(Insured insured) {
+        this.insured = insured;
+    }
+
+    public Timeslot getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(Timeslot timeslot) {
+        this.timeslot = timeslot;
+    }
 }
+
