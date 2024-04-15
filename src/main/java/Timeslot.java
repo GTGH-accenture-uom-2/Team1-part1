e2f7456d4b0e85cd9860ed7d831405fbb2090810
antoniapr <ad.protopsalti@gmail.com>
2024-04-15 08:57:44 +0300

    updates

--- src/main/java/Timeslot.java
+++ src/main/java/Timeslot.java
@@ -1,7 +1,11 @@
+/* Κλάση: Για τη χρονική θυρίδα εμβολιασμού  */
+
+import java.time.LocalDate;
+
 public class Timeslot {
-    private int day;
-    private int month;
-    private int year;
+    private LocalDate day;
+    private LocalDate month;
+    private LocalDate year;
     private int hour;
     private int minutes;
     private int startMinute;
@@ -9,7 +13,7 @@ public class Timeslot {
     private Doctor doctor;
 
     //Constructor No. 1
-    public Timeslot(int day, int month, int year, int hour,
+    public Timeslot(LocalDate day, LocalDate month, LocalDate year, int hour,
                     int minutes, int startMinute, int endMinute,
                     Doctor doctor){
         this.day = day;
@@ -21,31 +25,30 @@ public class Timeslot {
         this.endMinute = endMinute;
         this.doctor = doctor;
     }
+    //Constructor No. 2
+    public Timeslot(){}
+
     // Getters and setters
-    public int getDay() {
+    public LocalDate getDay() {
         return day;
     }
 
-    public void setDay(int day) {
+    public void setDay(LocalDate day) {
         this.day = day;
     }
 
-    public int getMonth() {
+    public LocalDate getMonth() {
         return month;
     }
-
-    public void setMonth(int month) {
+    public void setMonth(LocalDate month) {
         this.month = month;
     }
-
-    public int getYear() {
+    public LocalDate getYear() {
         return year;
     }
-
-    public void setYear(int year) {
+    public void setYear(LocalDate year) {
         this.year = year;
     }
-
     public int getHour() {
         return hour;
     }
@@ -85,6 +88,13 @@ public class Timeslot {
     public void setDoctor(Doctor doctor) {
         this.doctor = doctor;
     }
+
+    public String toString() {
+        return ("\nDay: " + day + ", \nMonth: " + month + ", \nYear: " + year + ", " +
+                "\nHour: " + hour + ", \nMinutes: " + minutes + ", " +
+                "\nStartMinute: " + startMinute + ", \nEndMinute: " + endMinute);
+    }
+
 
 }
 
